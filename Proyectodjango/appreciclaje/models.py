f15fad79540c949e81c1057cec5415ffa13f480a
seleluciano <seleluciano4@gmail.com>
2024-02-01 21:25:53 -0300

    boostrap

--- Proyectodjango/appreciclaje/models.py
+++ Proyectodjango/appreciclaje/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Cliente(models.Model):
     nombre=models.CharField(max_length=100)
     apellido=models.CharField(max_length=100)
-    DNI=models.IntegerField(max_length=10)
-    telefono=models.IntegerField(max_length=100)
+    DNI=models.IntegerField()
+    telefono=models.IntegerField()
     email=models.EmailField()
 
 class Administrador(models.Model):
